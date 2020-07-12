5fcea6c1cae442025df492f83c078ab0d943e0e3
Lee moon soo <leemoonsoo@gmail.com>
2020-07-12 09:38:01 -0700

    print table

--- cmd/starctl/starctl.go
+++ cmd/starctl/starctl.go
@@ -29,6 +29,50 @@ func CreateClient() *api.StaroidClient {
 	return &client
 }
 
+func PrintTable(header *[]string, rows *[]*[]string) {
+	colMax := make([]int, len(*header))
+
+	// get max length of each colume value
+	for _, row := range *rows {
+		for i, col := range *row {
+			if len(col) > colMax[i] {
+				colMax[i] = len(col)
+			}
+			if len((*header)[i]) > colMax[i] {
+				colMax[i] = len((*header)[i])
+			}
+		}
+	}
+
+	// build format string
+	formatString := ""
+	for i, colLen := range colMax {
+		fs := fmt.Sprintf("%%-%ds", colLen)
+		if i == 0 {
+			formatString = fs
+		} else {
+			formatString = fmt.Sprintf("%s  %s", formatString, fs)
+		}
+	}
+	formatString = fmt.Sprintf("%s\n", formatString)
+
+	// print header
+	headerArgs := make([]interface{}, len(*header))
+	for i, v := range *header {
+		headerArgs[i] = v
+	}
+	fmt.Printf(formatString, headerArgs...)
+
+	// print rows
+	for _, row := range *rows {
+		rowArgs := make([]interface{}, len(*row))
+		for i, v := range *row {
+			rowArgs[i] = v
+		}
+		fmt.Printf(formatString, rowArgs...)
+	}
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		usage()
